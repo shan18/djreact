a74e52941e840772be630a1927f4e71ba4209bc9
Shantanu <thegeek.004@gmail.com>
2018-12-11 20:35:55 +0530

    Combine all generic api views into a viewset

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,18 +1,25 @@
-from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    ArticleListView,
-    ArticleCreateView,
-    ArticleUpdateView,
-    ArticleDeleteView,
-    ArticleDetailView
-)
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, base_name='articles')
+urlpatterns = router.urls
 
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('create/', ArticleCreateView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-    path('<pk>/update/', ArticleUpdateView.as_view()),
-    path('<pk>/delete/', ArticleDeleteView.as_view())
-]
+# from django.urls import path
+
+# from .views import (
+#     ArticleListView,
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDeleteView,
+#     ArticleDetailView
+# )
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+#     path('<pk>/update/', ArticleUpdateView.as_view()),
+#     path('<pk>/delete/', ArticleDeleteView.as_view())
+# ]
